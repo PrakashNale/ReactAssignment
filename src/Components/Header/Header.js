24add67a9ec5620c3190dfeee2666e9e647e24a0
PrakashNale <30407849+PrakashNale@users.noreply.github.com>
2017-07-27 16:56:11 +0530

    Update Header.js

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -26,7 +26,7 @@ export class HeaderComponent extends React.Component {
                           <p>Share your TODO list here</p>
                            <nav>
                             <ul className="nav nav-pills">
-                              <li><Link to="/Home">Home</Link></li>
+                              <li><Link to="/">Home</Link></li>
                               <li><Link to="/About">About</Link></li>
                               <li><Link to="/createTask">Create Task</Link></li>
                             </ul>
